41705a798dd8981f86c16abd403ae3af71007e0a
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-02 23:41:08 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,82 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
-import { Movies } from "./components/Movies";
-import { useMovies } from "./hooks/useMovies";
+import { Movies } from './components/Movies';
+import { useMovies } from './hooks/useMovies';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import debounce from 'just-debounce-it';
+import './App.css';
 
 function useSearch() {
-  const [search, updateSearch] = useState("");
-  const [error, setError] = useState(null);
-  const isFirstInput = useRef(true);
+	const [search, updateSearch] = useState('');
+	const [error, setError] = useState(null);
+	const isFirstInput = useRef(true);
 
-  useEffect(() => {
-    if (isFirstInput.current) {
-      isFirstInput.current = search === "";
-      return;
-    }
-    if (search === "") {
-      setError("No se puede buscar una película vacía");
-      return;
-    }
+	useEffect(() => {
+		if (isFirstInput.current) {
+			isFirstInput.current = search === '';
+			return;
+		}
+		if (search === '') {
+			setError('No se puede buscar una película vacía');
+			return;
+		}
 
-    if (search.length < 3) {
-      setError("La búsqueda debe tener al menos 3 caracteres");
-      return;
-    }
+		if (search.length < 3) {
+			setError('La búsqueda debe tener al menos 3 caracteres');
+			return;
+		}
 
-    setError(null);
-  }, [search]);
+		setError(null);
+	}, [search]);
 
-  return { search, updateSearch, error };
+	return { search, updateSearch, error };
 }
 
 function App() {
-  const [sort, setSort] = useState(false);
-  const { search, updateSearch, error } = useSearch();
-  const { movies, getMovies } = useMovies({ search, sort });
+	const [sort, setSort] = useState(false);
+	const { search, updateSearch, error } = useSearch();
+	const { movies, getMovies, loading } = useMovies({ search, sort });
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    getMovies({ search });
-  };
+	const debouncedGetMovies = useCallback(
+		debounce(search => {
+			getMovies({ search });
+		}, 300),
+		[getMovies]
+	);
 
-  const handleChange = (event) => {
-    const newSearch = event.target.value;
-    updateSearch(newSearch);
-  };
+	const handleSubmit = event => {
+		event.preventDefault();
+		getMovies({ search });
+	};
 
-  return (
-    <div className="page">
-      <header>
-        <h1>Buscador de peliculas</h1>
-        <form onSubmit={handleSearch}>
-          <input
-            onChange={handleChange}
-            value={search}
-            name="query"
-            type="text"
-            placeholder="Matrix, marvel..."
-          />
-          <button type="submit">Search</button>
-        </form>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </header>
-      <main>
-        <Movies movies={movies} />
-      </main>
-    </div>
-  );
+	const handleSort = () => {
+		setSort(!sort);
+	};
+
+	const handleChange = event => {
+		const newSearch = event.target.value;
+		updateSearch(newSearch);
+		debouncedGetMovies(newSearch);
+	};
+
+	return (
+		<div className='page'>
+			<header>
+				<h1>Buscador de películas</h1>
+				<form className='form' onSubmit={handleSubmit}>
+					<input
+						name='query'
+						onChange={handleChange}
+						value={search}
+						placeholder='Avengers, matrix...'
+					/>
+					<input type='checkbox' onChange={handleSort} checked={sort} />
+					<button>Buscar</button>
+				</form>
+				{error && <p style={{ color: 'red' }}>{error}</p>}
+			</header>
+
+			<main>{loading ? <p>Loading...</p> : <Movies movies={movies} />}</main>
+		</div>
+	);
 }
 
 export default App;
--- src/services/movies.js
+++ src/services/movies.js
@@ -10,14 +10,12 @@ export const searchMovies = async ({ search }) => {
 
     const movies = json.Search;
 
-    return movies.map((movie) => {
-      return {
-        id: movie.imdbID,
-        title: movie.Title,
-        year: movie.Year,
-        poster: movie.Poster,
-      };
-    });
+    return movies?.map((movie) => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster,
+    }));
   } catch (error) {
     throw new Error("Error searching movies", error);
   }
