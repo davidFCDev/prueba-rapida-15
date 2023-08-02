d864e9ca89f67ee7df2ae0679f665215df6bb180
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-02 23:27:33 +0200

    gestion eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
 function App() {
+  const [search, updateSearch] = useState("");
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -14,12 +16,28 @@ function App() {
     };
   });
 
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form>
-          <input type="text" placeholder="Matrix, marvel..." />
+        <form onSubmit={handleSearch}>
+          <input
+            onChange={handleChange}
+            value={search}
+            name="query"
+            type="text"
+            placeholder="Matrix, marvel..."
+          />
           <button type="submit">Search</button>
         </form>
       </header>
